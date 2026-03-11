c4ede87d4f368a7ca72d0ab0c0f24b63b2e382ef
Prerna3738 <50740758+Prerna3738@users.noreply.github.com>
2026-03-10 23:28:14 -0400

    Strategy Implemented

--- src/main/java/ca/yorku/cmg/lob/stockexchange/tradingagent/TradingAgent.java
+++ src/main/java/ca/yorku/cmg/lob/stockexchange/tradingagent/TradingAgent.java
@@ -25,7 +25,7 @@ public abstract class TradingAgent {
     public void onEvent(Event e, int price) {
         int currentPos = positions.getOrDefault(e.getSecurity(), 0);
         if (strategy != null) {
-            strategy.actOnEvent(e, currentPos, price, this.trader, this.exc);
+            strategy.actOnEvent(e, currentPos, price, this.t, this.exc);
         }
     }
 
