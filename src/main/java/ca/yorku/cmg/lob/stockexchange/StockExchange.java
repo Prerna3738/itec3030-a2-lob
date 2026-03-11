aaf576b8dfac26791ff81db1ea6349c905a06915
Prerna3738 <50740758+Prerna3738@users.noreply.github.com>
2026-03-11 19:05:47 -0400

    Update TradingAgent creation to use newsDesk

--- src/main/java/ca/yorku/cmg/lob/stockexchange/StockExchange.java
+++ src/main/java/ca/yorku/cmg/lob/stockexchange/StockExchange.java
@@ -184,7 +184,7 @@ public class StockExchange {
 	                    }
 	                    // We now delegate the creation to our Factory
                         TradingAgentFactory factory = new TradingAgentFactory();
-                        TradingAgent agent = factory.createAgent(traderType, tradingStyle, t, this, nb);
+                        TradingAgent agent = factory.createAgent(traderType, tradingStyle, t, this, newsDesk);
 	                    
 	                } 
 					    if (agent != null) {
