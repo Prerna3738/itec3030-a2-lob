337ecd66009859c4c16485fc958a0affdb25e609
Prerna3738 <50740758+Prerna3738@users.noreply.github.com>
2026-03-11 20:06:14 -0400

    AggressiveStrategy ChangesImplemented

--- src/main/java/ca/yorku/cmg/lob/stockexchange/strategy/AggressiveStrategy.java
+++ src/main/java/ca/yorku/cmg/lob/stockexchange/strategy/AggressiveStrategy.java
@@ -11,10 +11,11 @@ public class AggressiveStrategy implements ITradingStrategy {
     @Override
     public void actOnEvent(Event e, int pos, int price, Trader t, StockExchange exc) {
         IOrder newOrder = null;
+        int quantity = 100;
         if (e instanceof GoodNews) {
-            newOrder = new Bid(t, e.getSecrity(), (int)Math.round(price * 1.05), (int)Math.round(pos * 0.5), e.getTime());
+            newOrder = new Bid(t, e.getSecrity(), (int)Math.round(price * 1.05), quantity, e.getTime());
         } else if (e instanceof BadNews) {
-            newOrder = new Ask(t, e.getSecrity(), (int)Math.round(price * 0.90), (int)Math.round(pos * 0.8), e.getTime());
+            newOrder = new Ask(t, e.getSecrity(), (int)Math.round(price * 0.90), quantity, e.getTime());
         }
         if (newOrder != null) exc.submitOrder(newOrder, e.getTime());
     }
