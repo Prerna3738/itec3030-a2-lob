a67d7f1ed0741eaf96965729ad620150912cad36
Prerna3738 <50740758+Prerna3738@users.noreply.github.com>
2026-03-12 14:56:14 -0400

    Rename package from strategy to tradingagent

--- src/main/java/ca/yorku/cmg/lob/stockexchange/strategy/ITradingStrategy.java
+++ src/main/java/ca/yorku/cmg/lob/stockexchange/strategy/ITradingStrategy.java
@@ -1,4 +1,4 @@
-package ca.yorku.cmg.lob.stockexchange.strategy;
+package ca.yorku.cmg.lob.stockexchange.tradingagent;
 
 import ca.yorku.cmg.lob.stockexchange.StockExchange;
 import ca.yorku.cmg.lob.stockexchange.events.Event;
